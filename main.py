a0a40166a3abd5aa05a4ca055139f317cd75872e
Icestab <dengyue35@qq.com>
2024-07-07 15:38:59 +0800

    通过main启动

---
+++ main.py
@@ -0,0 +1,20 @@
+# 导入必要的模块
+import logging
+import sys
+
+# 从其他模块导入类和函数
+from cos import Cos
+
+
+# 定义函数
+def main():
+    # 配置日志记录
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+    cos = Cos()
+    cos.upload()
+
+
+# 当文件作为脚本运行时执行
+if __name__ == "__main__":
+    main()
