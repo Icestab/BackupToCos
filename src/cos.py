14c18e02e9851165a640ec352c78378f6f37efee
Icestab <dengyue35@qq.com>
2024-07-07 19:48:06 +0800

    添加检测环境变量

--- src/cos.py
+++ src/cos.py
@@ -7,23 +7,28 @@ import logging
 import crc64
 
 # 正常情况日志级别使用 INFO，需要定位时可以修改为 DEBUG，此时 SDK 会打印和服务端的通信信息
-logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
 
 class Cos:
     def __init__(self):
-        # 1. 设置用户属性, 包括 secret_id, secret_key, region等。Appid 已在 CosConfig 中移除，请在参数 Bucket 中带上 Appid。Bucket 由 BucketName-Appid 组成
-        self.secret_id = os.environ[
-            "COS_SECRET_ID"
-        ]  # 用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
-        self.secret_key = os.environ[
-            "COS_SECRET_KEY"
-        ]  # 用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
-        self.region = os.environ[
-            "COS_REGION"
-        ]  # 替换为用户的 region，已创建桶归属的 region 可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket
-        # COS 支持的所有 region 列表参见 https://cloud.tencent.com/document/product/436/6224
+        try:
+            # 1. 设置用户属性, 包括 secret_id, secret_key, region等。Appid 已在 CosConfig 中移除，请在参数 Bucket 中带上 Appid。Bucket 由 BucketName-Appid 组成
+            self.secret_id = os.environ[
+                "COS_SECRET_ID"
+            ]  # 用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
+            self.secret_key = os.environ[
+                "COS_SECRET_KEY"
+            ]  # 用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
+            self.region = os.environ[
+                "COS_REGION"
+            ]  # 替换为用户的 region，已创建桶归属的 region 可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket
+            # COS 支持的所有 region 列表参见 https://cloud.tencent.com/document/product/436/6224
+            self.uploadName = os.environ["COS_UPLOAD_NAME"]
+            self.Bucket = os.environ["COS_BUCKET"]
+        except KeyError:
+            logger.error("请检查环境变量是否配置正确")
+            exit()
         self.token = None  # 如果使用永久密钥不需要填入 token，如果使用临时密钥需要填入，临时密钥生成和使用指引参见 https://cloud.tencent.com/document/product/436/14048
         self.scheme = (
             "https"  # 指定使用 http/https 协议来访问 COS，默认为 https，可不填
@@ -37,8 +42,6 @@ class Cos:
             Scheme=self.scheme,
         )
         self.client = CosS3Client(self.config)
-        self.uploadName = os.environ["COS_UPLOAD_NAME"]
-        self.Bucket = os.environ["COS_BUCKET"]
         self.crc64 = crc64.CRC64(self.uploadName)
         self.local_crc64ecma = self.crc64.calculate_crc64()
 
